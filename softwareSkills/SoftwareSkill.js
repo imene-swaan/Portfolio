7b9663a63e82215c5c2315bb496770cc219ce139
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2021-09-11 00:45:22 +0100

    Add files via upload

---
+++ softwareSkills/SoftwareSkill.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./SoftwareSkill.css";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
+class SoftwareSkill extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="software-skills-main-div">
+          <ul className="dev-icons">
+            {/* {skillsSection.softwareSkills.map(skills => {
+            return (
+              <li className="software-skill-inline" name={skills.skillName}>
+                <i className={skills.fontAwesomeClassname}></i>
+              </li>
+            );
+          })} */}
+            {this.props.logos.map((logo) => {
+              return (
+                <OverlayTrigger
+                  key={logo.skillName}
+                  placement={"top"}
+                  overlay={
+                    <Tooltip id={`tooltip-top`}>
+                      <strong>{logo.skillName}</strong>
+                    </Tooltip>
+                  }
+                >
+                  <li className="software-skill-inline" name={logo.skillName}>
+                  <img src={require(`../../assets/icons/${logo.icon_url}`)} width="60px" alt={logo.skillName}/>
+                  </li>
+                </OverlayTrigger>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SoftwareSkill;
